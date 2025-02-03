b968376d0f48b55d2462113c0457063e16abe305
Maksym Buslenko <maksbusl@gmail.com>
2025-02-03 12:46:17 +0200

    use orjson

--- ai-application/main.py
+++ ai-application/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
 from core.models import db_helper
+from fastapi.responses import ORJSONResponse
 
 
 
@@ -17,11 +18,11 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 main_app = FastAPI(
+    default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
 main_app.include_router(
     api_router,
-    prefix=settings.api.prefix
 )
 
 
