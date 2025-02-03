45a4eb910d798ee281cb2eb1ba06e9c3dc28b184
Maksym Buslenko <maksbusl@gmail.com>
2025-02-03 21:45:50 +0200

    create User model

--- ai-application/core/models/user.py
+++ ai-application/core/models/user.py
@@ -1,10 +1,13 @@
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from collections.abc import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from core.models.mixins.int_id_pk import IntIdPkMixin
 from .base import Base
-from .mixins.int_id_pk import IntIdPkMixin
 
 
-class User(IntIdPkMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
-
-
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+    pass
