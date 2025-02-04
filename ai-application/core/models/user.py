e91dda170318845aa227165f3d63ee58b90affb7
Maksym Buslenko <maksbusl@gmail.com>
2025-02-04 18:21:18 +0200

    create migration for users table

--- ai-application/core/models/user.py
+++ ai-application/core/models/user.py
@@ -1,10 +1,4 @@
-from collections.abc import AsyncGenerator
-
-from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
-
 from core.models.mixins.int_id_pk import IntIdPkMixin
 from .base import Base
 
