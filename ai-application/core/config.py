79cd21bd5d914a65a7c54fde98b6354b42557b29
Maksym Buslenko <maksbusl@gmail.com>
2025-02-07 23:05:12 +0200

    create auth router

--- ai-application/core/config.py
+++ ai-application/core/config.py
@@ -7,9 +7,11 @@ class RunConfig(BaseModel):
     host: str = "127.0.0.1"
     port: int = 8000
 
+
 class ApiV1Prefix(BaseModel):
     prefix: str = "/v1"
-    users: str = "/users"
+    auth: str = "/auth"
+
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
@@ -23,13 +25,14 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
-    naming_convention: dict[str,str] = {
+    naming_convention: dict[str, str] = {
         "ix": "ix_%(column_0_label)s",
         "uq": "uq_%(table_name)s_%(column_0_N_name)s",
         "ck": "ck_%(table_name)s_%(constraint_name)s",
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s"
+        "pk": "pk_%(table_name)s",
     }
+
 
 class AccessToken(BaseModel):
     lifetime_seconds: int = 3600
