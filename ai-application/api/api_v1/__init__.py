7a4ca4d5a2592d98e0fc30193c996b8a9cc98d0b
Maksym Buslenko <maksbusl@gmail.com>
2025-02-08 23:04:54 +0200

    bearer transport path from config

--- ai-application/api/api_v1/__init__.py
+++ ai-application/api/api_v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from core.config import settings
-from auth import router as auth_router
+from .auth import router as auth_router
 
 router = APIRouter(prefix=settings.api.v1.prefix)
 
