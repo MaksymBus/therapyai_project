4ecf73a5a7523efce6a809cbf10bd009dcb76de0
Maksym Buslenko <maksbusl@gmail.com>
2025-02-09 23:09:55 +0200

    add register route

--- ai-application/api/api_v1/auth.py
+++ ai-application/api/api_v1/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from core.config import settings
 from api.dependencies.authentication.backend import authentication_backend
 from api.api_v1.fastapi_users_router import fastapi_users
+from core.schemas.user import UserRead, UserCreate
 
 
 router = APIRouter(
@@ -9,6 +10,13 @@ router = APIRouter(
     tags=["Auth"],
 )
 
+# /login
+# /logout
 router.include_router(
     router=fastapi_users.get_auth_router(authentication_backend),
+)
+
+# /register
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
 )
--- ai-application/api/dependencies/authentication/access_tokens.py
+++ ai-application/api/dependencies/authentication/access_tokens.py
@@ -8,11 +8,7 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_access_token_db(session: Annotated["AsyncSession",
-Depends(db_helper.session_getter),
-    ],
+async def get_access_token_db(
+    session: Annotated["AsyncSession", Depends(db_helper.session_getter)],
 ):
     yield AccessToken.get_db(session=session)
-
-
-
