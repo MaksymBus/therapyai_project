f608cb1059b60a19a456262515a61c3936392ba6
Maksym Buslenko <maksbusl@gmail.com>
2025-02-10 14:13:28 +0200

    add routers for user password reset

--- ai-application/api/api_v1/auth.py
+++ ai-application/api/api_v1/auth.py
@@ -18,11 +18,17 @@ router.include_router(
 
 # /register
 router.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
+    router=fastapi_users.get_register_router(UserRead, UserCreate),
 )
 
 # /request-verify-token
 # /verify
 router.include_router(
-    fastapi_users.get_verify_router(UserRead),
+    router=fastapi_users.get_verify_router(UserRead),
+)
+
+# /forgot-password
+# /reset-password
+router.include_router(
+    router=fastapi_users.get_reset_password_router(),
 )
